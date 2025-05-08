d27682ac07057920a969d3e45c7fd8f18909777b
Golluméo <p.mauriello54@gmail.com>
2025-05-08 17:05:55 +0200

    feat(categorizer): implement default transaction categorization
    Adds `DefaultCategorizeTransaction` class to categorize transactions by food delivery, transport, high expense, or uncategorized.
    Introduces the `ICategorizeTransaction` interface for abstraction and maintainability.
    Supports keyword detection and expense-based categorization logic.

---
+++ Ledger.Categorizer.Application/Contracts/ICategorizeTransaction.cs
@@ -0,0 +1,9 @@
+﻿using Ledger.Categorizer.Domain.Entities;
+using Ledger.Categorizer.Domain.ValueObjects;
+
+namespace Ledger.Categorizer.Application.Contracts;
+
+public interface ICategorizeTransaction
+{
+    Category Categorize(Transaction transaction);
+}
---
+++ Ledger.Categorizer.Infrastructure/DefaultCategorizeTransaction.cs
@@ -0,0 +1,41 @@
+﻿using Ledger.Categorizer.Application.Contracts;
+using Ledger.Categorizer.Domain.Entities;
+using Ledger.Categorizer.Domain.ValueObjects;
+
+namespace Ledger.Categorizer.Infrastructure;
+
+public class DefaultCategorizeTransaction : ICategorizeTransaction
+{
+    private static readonly string[] FoodDeliveryKeywords = ["eats", "deliveroo"];
+    private static readonly string[] TransportKeywords = ["uber", "taxi"];
+
+
+    public Category Categorize(Transaction transaction)
+    {
+        if (IsFoodDelivery(transaction)) return Category.FoodDelivery;
+
+        if (IsHighExpense(transaction)) return Category.HighExpense;
+
+        if (IsTransport(transaction)) return Category.Transport;
+
+        return Category.Uncategorized;
+    }
+
+    private static bool IsFoodDelivery(Transaction transaction)
+    {
+        var description = transaction.Description.ToLowerInvariant();
+        return FoodDeliveryKeywords.Any(k => description.Contains(k));
+    }
+
+    private static bool IsHighExpense(Transaction transaction)
+    {
+        return transaction.Amount >= 1000;
+    }
+
+    private static bool IsTransport(Transaction transaction)
+    {
+        var description = transaction.Description.ToLowerInvariant();
+        return TransportKeywords.Any(transportKeyWord => description.Contains(transportKeyWord)) &&
+               !FoodDeliveryKeywords.Any(foodDeliveryKeyword => description.Contains(foodDeliveryKeyword));
+    }
+}
